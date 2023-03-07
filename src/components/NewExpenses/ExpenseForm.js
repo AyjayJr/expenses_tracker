4c35ab8184d349b0e063c55e3d163a39ed4c5bb7
AyjayJr <ayjayjr@hotmail.com>
2023-03-07 08:33:31 -0500

    experimenting with different ways to call use state in expenseForm

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -3,14 +3,38 @@ import './ExpenseForm.css';
 
 function ExpenseForm() {
 	// we always use a string here bc e.target.value is always stored as a string
+	// this is an example of using muliple state calls as opposed to just one state call
 	const [userTitle, setUserTitle] = useState('');
 	const [userAmount, setUserAmount] = useState('');
 	const [userDate, setUserDate] = useState('');
+
+	// to use store multiple variables in one state call use and object
+
+	// const [userInput, setUserInput] = useState({
+	// 	userTitle: '',
+	// 	userAmount: '',
+	// 	userDate: ''
+	// });
 
 	// event is a default js argument that has details about the event that occurred
 	// we are interested in the value property which stores the value in the input field
 	const titleChangeHandler = (event) => {
 		setUserTitle(event.target.value);
+
+		// setting userinput when there is only one state means u must ensure the other info doesn't get lost
+		// here we use spread operator to copy all the other info and then override the title with the new information
+		// this creates an issue where we are depending on a previous state
+
+		// setUserInput({
+		// 	...userInput,
+		// 	userTitle: event.target.value
+		// });
+
+		// proper way to set states when you are depending on a previous state
+
+		// setUserInput((prevState) => {
+		// 	return {...prevState, userTitle: event.target.value};
+		// });
 	};
 
 	const amountChangeHandler = (event) => {
