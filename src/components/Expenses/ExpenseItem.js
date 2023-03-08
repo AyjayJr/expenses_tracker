b622d2d9de21e45643642c3df73a3c2275bb547f
AyjayJr <ayjayjr@hotmail.com>
2023-03-08 15:09:18 -0500

    changed from static list of expense items to dynamical list

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,6 +4,7 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+	/*
 	// useState allows us to change variables that have already been rendered
 	// must pass the variable that wants to change
 	// useState returns 2 variables, the variable to be changed, and a function to change it
@@ -12,15 +13,16 @@ function ExpenseItem(props) {
 	const clickHandler= () => {
 		setTitle('Updated!');
 	}
+	*/
 
 	return (
 		<Card className="expense-item">
 			<ExpenseDate date={props.date} />
 			<div className="expense-item__description">
-				<h2>{title}</h2>
+				<h2>{props.title}</h2>
 				<div className="expense-item__price">${props.amount}</div>
 			</div>
-			<button onClick={clickHandler}>Change Title</button>
+			{/* <button onClick={clickHandler}>Change Title</button> */}
 		</Card>
 	);
 }
