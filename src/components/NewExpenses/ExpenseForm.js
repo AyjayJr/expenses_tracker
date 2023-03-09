5a9aa2f190cb517103eab336a807d921f1e3ef3d
AyjayJr <ayjayjr@hotmail.com>
2023-03-09 10:53:00 -0500

    added buttons to show and hide the add expenses form

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -7,44 +7,13 @@ function ExpenseForm(props) {
 	const [userTitle, setUserTitle] = useState('');
 	const [userAmount, setUserAmount] = useState('');
 	const [userDate, setUserDate] = useState('');
-
-	// to use store multiple variables in one state call use and object
-
-	// const [userInput, setUserInput] = useState({
-	// 	userTitle: '',
-	// 	userAmount: '',
-	// 	userDate: ''
-	// });
+	const [showForm, setShowForm] = useState(false);
 
 	// event is a default js argument that has details about the event that occurred
 	// we are interested in the value property which stores the value in the input field
-	const titleChangeHandler = (event) => {
-		setUserTitle(event.target.value);
-
-		// setting userinput when there is only one state means u must ensure the other info doesn't get lost
-		// here we use spread operator to copy all the other info and then override the title with the new information
-		// this creates an issue where we are depending on a previous state
-
-		// setUserInput({
-		// 	...userInput,
-		// 	userTitle: event.target.value
-		// });
-
-		// proper way to set states when you are depending on a previous state
-		// must use an arrow function that receives the previous state and returns the new one
-
-		// setUserInput((prevState) => {
-		// 	return {...prevState, userTitle: event.target.value};
-		// });
-	};
-
-	const amountChangeHandler = (event) => {
-		setUserAmount(event.target.value);
-	};
-
-	const dateChangeHandler = (event) => {
-		setUserDate(event.target.value);
-	};
+	const titleChangeHandler = (event) => { setUserTitle(event.target.value); };
+	const amountChangeHandler = (event) => { setUserAmount(event.target.value); };
+	const dateChangeHandler = (event) => { setUserDate(event.target.value); };
 
 	const submitHandler = (event) => {
 		// this stops the page from sending a request and reloading when the form submit button is pressed
@@ -64,26 +33,40 @@ function ExpenseForm(props) {
 		setUserDate('');
 	};
 
+	const openFormHandler = () => { setShowForm(true); };
+	const closeFormHandler = () => { setShowForm(false); };
+
+	let content = <button onClick={openFormHandler}>Add New Expense</button>
+
+	if (showForm) {
+		content = (
+			<form onSubmit={submitHandler}>
+				<div className='new-expense__controls'>
+					<div className='new-expense__control'>
+						<label>Title</label>
+						<input type='text' value={userTitle} onChange={titleChangeHandler} />
+					</div>
+					<div className='new-expense__control'>
+						<label>Amount</label>
+						<input type='number' min='0.01' value={userAmount} step='0.01' onChange={amountChangeHandler} />
+					</div>
+					<div className='new-expense__control'>
+						<label>Date</label>
+						<input type='date' min='2019-01-01' value={userDate} max='2023-12-31' onChange={dateChangeHandler} />
+					</div>
+				</div>
+				<div className="new-expense__actions">
+					<button onClick={closeFormHandler}>Cancel</button>
+					<button type='submit'>Add Expense</button>
+				</div>
+			</form>
+		);
+	}
+
 	return (
-		<form onSubmit={submitHandler}>
-			<div className='new-expense__controls'>
-				<div className='new-expense__control'>
-					<label>Title</label>
-					<input type='text' value={userTitle} onChange={titleChangeHandler} />
-				</div>
-				<div className='new-expense__control'>
-					<label>Amount</label>
-					<input type='number' min='0.01' value={userAmount} step='0.01' onChange={amountChangeHandler} />
-				</div>
-				<div className='new-expense__control'>
-					<label>Date</label>
-					<input type='date' min='2019-01-01' value={userDate} max='2023-12-31' onChange={dateChangeHandler} />
-				</div>
-			</div>
-			<div className="new-expense__actions">
-				<button type='submit'>Add Expense</button>	
-			</div>
-		</form>
+		<div>
+			{content}
+		</div>
 	)
 }
 
