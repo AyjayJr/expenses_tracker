d0f91979bffb76a57e020cca4f89ed50b411b297
AyjayJr <ayjayjr@hotmail.com>
2023-03-09 10:10:32 -0500

    added expenses list component that has a condition return value based on the amount of items in the expenses list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
+import React, { useState } from "react";
+import ExpensesList from "./ExpensesList";
 import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 import './Expenses.css';
@@ -13,23 +13,12 @@ function Expenses(props) {
 
   const filteredExpenses = props.expenses.filter(expense => expense.date.getFullYear().toString() === year);
 
-  let expensesContent = <p>No expenses found.</p>
 
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ))
-  }
 
   return (
     <Card className="expenses">
       <ExpensesFilter selected={year} onFilterSelect={filterSelectHandler} />
-      {expensesContent}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   )
 }
